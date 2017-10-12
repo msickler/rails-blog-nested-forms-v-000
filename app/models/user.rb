9499d07ba5ad26e9f0a9d6bfae5f5cd56ae0614b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-12 23:37:10 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   accepts_nested_attributes_for :posts, reject_if: proc { |attributes| attributes['title'].blank? }
-  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['tag'].blank? }
 
   validates_uniqueness_of :name
   validates_presence_of :name
